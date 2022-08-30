f42577d20f0f2a0ae9d410cb6320c53081d642af
Jigsy <34282672+Jigsy1@users.noreply.github.com>
2022-08-30 03:50:43 +0000

    Add files via upload
    I actually confused my camelCased #defines with variables. Therefore, I've decided to simply follow the proper standard rather than what I think I should be doing.
    
    Also changed a few minor comments.
    
    There will be no further changes to this.

--- C/twirl.c
+++ C/twirl.c
@@ -8,36 +8,38 @@
  * ...> tcc -c twirl.c
  * ...> tcc -run twirl.o
  *
- * NOTE: You will need to CTRL+C (^C) to terminate the program.
+ *
+ * Note: You will need to do CTRL+C to terminate the program in the Command Prompt.
+ *
  */
 
 #include <stdio.h>
 #include <winapi/windows.h>
 
-#define sleepTime 500
-// 500 = 0.5s on Windows
+#define SLEEP_TIME 500
+// `-> 500 is equal to 0.5s on Windows.
 
 int main(void) {
 	int twirlCount = 1;
 	for (;;) {
-		// Infinite loop
+		// `-> Infinite loop.
 		switch(twirlCount) {
 			case 1:
 				printf("\b|");
 				twirlCount += 1;
-				Sleep(sleepTime);
+				Sleep(SLEEP_TIME);
 			case 2:
 				printf("\b/");
 				twirlCount += 1;
-				Sleep(sleepTime);
+				Sleep(SLEEP_TIME);
 			case 3:
 				printf("\b-");
 				twirlCount += 1;
-				Sleep(sleepTime);
+				Sleep(SLEEP_TIME);
 			case 4:
 				printf("\b\\");
-				twirlCount = 1;		// Reset
-				Sleep(sleepTime);
+				twirlCount = 1;		// -> Reset.
+				Sleep(SLEEP_TIME);
 		}
 	}
 	return 0;
