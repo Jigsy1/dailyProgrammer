a6d0c9b1062fae93394673faa2ad2efecc182194
Jigsy <34282672+Jigsy1@users.noreply.github.com>
2022-12-28 22:53:08 +0000

    Add files via upload

---
+++ py3/fizzBuzz.py
@@ -0,0 +1,16 @@
+﻿# fizzBuzz.py by Jigsy (https://github.com/Jigsy1) released under the Unlicense.
+
+for thisLoop in range(1, 101):
+	# `-> Start at 1, end at 100.
+	if thisLoop % 5 == 0 and thisLoop % 3 == 0:
+		print("Fizz Buzz")
+		continue
+	if thisLoop % 5 == 0:
+		print("Buzz")
+		continue
+	if thisLoop % 3 == 0:
+		print("Fizz")
+		continue
+	print(thisLoop)
+
+# EOF
