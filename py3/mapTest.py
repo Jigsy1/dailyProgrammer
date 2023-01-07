4fa7464d58a9166edac7a0e01578c104b8a96e18
Jigsy <34282672+Jigsy1@users.noreply.github.com>
2023-01-07 01:02:32 +0000

    Add files via upload
    Added a hundred new items to the dictionary. Also tided up a lot.

--- py3/mapTest.py
+++ py3/mapTest.py
@@ -2,16 +2,22 @@
 #
 # If you've ever done /map on a large IRC server, you'll understand what this does. If you don't, see: https://i.imgur.com/2FNaI4A.png
 #
-# As far as I can tell, this works correctly. (Keywords: As far as I can tell.)
+# As far as I can tell, this works correctly. (Keywords: As far as I can tell)
+
 
 # define
 
+CHAR_PIPE = "¦"
+# CHAR_SPACE = " "
+CHAR_STUB = "-"
+CHAR_TAIL = "`"
 END_OF_MAP = "End of /MAP"
 
-# Tree
+
+# "Tree"
 
 servers = {
-        # ,-> "child": "parent"[,] - , isn't required for the final entry obviously.
+        # ,-> "child": "parent"[,] - The , isn't required for the final entry hence why it's in [].
         "angelicarogner.localhost": "towaherschel.localhost",
         "titarussell.localhost": "angelicarogner.localhost",
         "estellebright.localhost": "titarussell.localhost",
@@ -66,20 +72,105 @@ servers = {
         "gracelynn.localhost": "tioplato.localhost",
         "eliemacdowell.localhost": "tioplato.localhost",
         "mariabellcrois.localhost": "eliemacdowell.localhost",
-        # ,-> A few new (randomly placed?) entries!
+        # ,-> A few new hundred (randomly placed?) entries.
+        # ¦-> Let's just fill the entire list with more lovely ladies from the Trails of... Universe.
+        "suzanneegret.localhost": "musseegret.localhost",
         "ferridaalfayed.localhost": "fieclaussell.localhost",
         "risettetwinings.localhost": "ferridaalfayed.localhost",
         "agnesclaudel.localhost": "ferridaalfayed.localhost",
         "judithranster.localhost": "mariabellcrois.localhost",
         "lucreziaisselee.localhost": "risettetwinings.localhost",
         "marielleayme.localhost": "risettetwinings.localhost",
-        "yumeayme.localhost": "marielleayme.localhost"
+        "yumeayme.localhost": "marielleayme.localhost",
+        "linacrawford.localhost": "junacrawford.localhost",
+        "nanacrawford.localhost": "junacrawford.localhost",
+        "ariellenheim.localhost": "priscillareisearnor.localhost",
+        "auriervander.localhost": "towaherschel.localhost",
+        "marthaherschel.localhost": "towaherschel.localhost",
+        "fatimaworzel.localhost": "towaherschel.localhost",
+        "sheedaworzel.localhost": "fatimaworzel.localhost",
+        "lilyworzel.localhost": "fatimaworzel.localhost",
+        "clariceseeker.localhost": "noelseeker.localhost",
+        "kilikarouran.localhost": "scherazardharvey.localhost",
+        "iseriaforst.localhost": "saravalestein.localhost",
+        "lavianwinslet.localhost": "iseriaforst.localhost",
+        "jaynastorm.localhost": "lavianwinslet.localhost",
+        "aliciavonauslese.localhost": "klaudiavonauslese.localhost",
+        "shanshan.localhost": "tioplato.localhost",
+        "theresia.localhost": "alisareinford.localhost",
+        "emily.localhost": "theresia.localhost",
+        "jingo.localhost": "ashleigh.localhost",
+        "ashleigh.localhost": "tioplato.localhost",
+        "lapisrosenberg.localhost": "angelicarogner.localhost",
+        "claravoce.localhost": "towaherschel.localhost",
+        "linde.localhost": "claravoce.localhost",
+        "vivi.localhost": "linde.localhost",
+        "beatrix.localhost": "saravalestein.localhost",
+        "sariffa.localhost": "ferrisflorald.localhost",
+        "kaelamacmillan.localhost": "judithranster.localhost",
+        "theresa.localhost": "klaudiavonauslese.localhost",
+        "polly.localhost": "theresa.localhost",
+        "mary.localhost": "theresa.localhost",
+        "sashapetrovna.localhost": "lucyseiland.localhost",
+        "tatiana.localhost": "towaherschel.localhost",
+        "monica.localhost": "laurasarseid.localhost",
+        "louise.localhost": "junacrawford.localhost",
+        "sandy.localhost": "alfinreisearnor.localhost",
+        "maybelle.localhost": "scherazardharvey.localhost",
+        "valerie.localhost": "saravalestein.localhost",
+        "patiry.localhost": "leonora.localhost",
+        "lotte.localhost": "patiry.localhost",
+        "annabelle.localhost": "lotte.localhost",
+        "scarlet.localhost": "annabelle.localhost",
+        "carna.localhost": "scarlet.localhost",
+        "dorothyhyatt.localhost": "estellebright.localhost",
+        "lila.localhost": "carna.localhost",
+        "luciola.localhost": "lila.localhost",
+        "paulette.localhost": "lila.localhost",
+        "nadiarayne.localhost": "lapisrosenberg.localhost",
+        "wendy.localhost": "paulette.localhost",
+        "kate.localhost": "eliemacdowell.localhost",
+        "hilda.localhost": "wendy.localhost",
+        "elsacochrane.localhost": "risettetwinings.localhost",
+        "najeberca.localhost": "kaelamacmillan.localhost",
+        "ninafenly.localhost": "najeberca.localhost",
+        "sophiahayworth.localhost": "rixiamao.localhost",
+        "shizunaremmisurugi.localhost": "hilda.localhost",
+        "lynn.localhost": "shizunaremmisurugi.localhost",
+        "aeolia.localhost": "lynn.localhost",
+        "maya.localhost": "tatiana.localhost",
+        "colette.localhost": "aeolia.localhost",
+        "mint.localhost": "titarussell.localhost",
+        "erikarussell.localhost": "titarussell.localhost",
+        "elaineauclair.localhost": "agnesclaudel.localhost",
+        "katarinaford.localhost": "shirleyorlando.localhost",
+        "anelaceelfead.localhost": "scherazardharvey.localhost",
+        "leonora.localhost": "towaherschel.localhost",
+        "adagrant.localhost": "shirleyorlando.localhost",
+        "paula.localhost": "colette.localhost",
+        "becky.localhost": "paula.localhost",
+        "beryl.localhost": "becky.localhost",
+        "dorothee.localhost": "emmamillstein.localhost",
+        "edel.localhost": "fieclaussell.localhost",
+        "friedel.localhost": "beryl.localhost",
+        "bridget.localhost": "mint.localhost",
+        "mirabelaalton.localhost": "friedel.localhost",
+        "jillridonor.localhost": "klaudiavonauslese.localhost",
+        "ainaholden.localhost": "scherazardharvey.localhost",
+        "chloebarnett.localhost": "hilda.localhost",
+        "juliaschwarz.localhost": "maya.localhost",
+        "celestedauslese.localhost": "juliaschwarz.localhost",
+        "esmerayarchette.localhost": "juliaschwarz.localhost",
+        "kanoneamalthea.localhost": "lucreziaisselee.localhost"
+        # ¦-> If you want to change these, feel free, but take care not to cause an endless loop.
+        # `-> If you do, press CTRL+C (at least on Windows) to break.
 }
+
 
 # Core
 
 def countProgeny(name):
-        """Counts and returns - if available - the number of progeny for an item."""
+        """Counts and returns the number of progeny for a name."""
         progeny = 0
         for child, parent in servers.items():
                 if name == parent:
@@ -87,66 +178,74 @@ def countProgeny(name):
         return progeny
 
 def makePrettyString(string, position):
-        """Hopefully cleans up dead branches."""
+        """Clean up tree branches that lead to nowhere."""
         newString = string
         newString = list(newString)
         stringLoop = 0
         while stringLoop < len(position):
                 tempNumber = int(position[stringLoop])
                 if tempNumber != -1:
-                        # ,-> This next line below can possibly be commented out. I'm not 100% sure.
+                        # ,-> This next line below can possibly be commented out. (I'm not 100% sure. I've decided to leave it in.)
                         if string[tempNumber] != " ":
-                                if string[tempNumber] != "`":
+                                if string[tempNumber] != CHAR_TAIL:
                                         newString[tempNumber] = " "
                 stringLoop += 1
         newString = "".join(newString)
         return newString
 
-def recurseMap(recursion, progeny, parentName, position):
-        loop = 0
+def recurseMap(recursion, progeny, parentName, tailPosition):
+        loopCount = 0
+        # `-> NOT THE SAME AS RECURSION!
         oldProgeny = progeny
         for child, parent in servers.items():
                 if parentName == parent:
-                        loop += 1
-                        newProgeny = countProgeny(child)
-                        string = "| " * recursion
-                        if newProgeny > 0:
-                                # ,-> This - next one - should probably be oldProgeny(?); but if it ain't fixed, don't broke it!
-                                if loop != progeny:
-                                        string = string + "|"
-                                else:
-                                        string = string + "`"
-                        else:
-                                if loop < oldProgeny:
-                                        string = string + "|"
-                                else:
-                                        string = string + "`"
-                        if string.rfind("`") != -1:
-                                position = position + " " + str(string.rfind("`"))
-                        tempPosition = list(position.split(" "))
-                        # Q. "Why didn't you just make a list to start with?"
-                        # A. Because it didn't return the string in the manner I needed it in order to get this to work correctly.
-                        string = makePrettyString(string, tempPosition) + "-" + child
-                        print(string)
+                        loopCount += 1
+                        progeny = countProgeny(child)
+                        string = (CHAR_PIPE + " ") * recursion
                         if progeny > 0:
-                                recurseMap((recursion + 1), newProgeny, child, position)
+                                if loopCount != oldProgeny:
+                                        # `-> This should cover both < and >.
+                                        string = string + CHAR_PIPE
+                                else:
+                                        string = string + CHAR_TAIL
+                        else:
+                                if loopCount < oldProgeny:
+                                        string = string + CHAR_PIPE
+                                else:
+                                        string = string + CHAR_TAIL
+                        if string.rfind(CHAR_TAIL) != -1:
+                                tailPosition = tailPosition + " " + str(string.rfind(CHAR_TAIL))
+                        tempPosition = list(tailPosition.split(" "))
+                        # Q. "Why didn't you just make a list to start with?"
+                        # A. Because it didn't return the string in the manner I needed it in order to get this to work correctly, it would keep appending to the list.
+                        #       However, setting a string variable like this resets the variable when I need it to.
+                        printString = makePrettyString(string, tempPosition) + CHAR_STUB + child
+                        print(printString)
+                        if progeny > 0:
+                                recurseMap((recursion + 1), progeny, child, tailPosition)
 
-def showMaps(ourServer):
+def showMaps(ourParent):
         """Print the entire map tree from a specified starting point."""
-        print(ourServer)
-        recurseMap(0, countProgeny(ourServer), ourServer, "-1")
-        # `-> DO NOT REMOVE THE "-1"!
+        print(ourParent)
+        progeny = countProgeny(ourParent)
+        if progeny > 0:
+                recurseMap(0, progeny, ourParent, "-1")
+                # `-> DO NOT CHANGE OR REMOVE THE "-1"!
         print(END_OF_MAP)
 
 def main():
         localServer = "towaherschel.localhost"
+        #
         # A few other test cases:
         #
+        # localServer = "priscillareisearnor.localhost"
+        # localServer = "annabelle.localhost"
         # localServer = "noelseeker.localhost"
         # localServer = "saravalestein.localhost"
         showMaps(localServer)
 
 if __name__  == "__main__":
         main()
+
 
 # EOF
