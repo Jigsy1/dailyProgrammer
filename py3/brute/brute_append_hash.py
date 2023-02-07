c4f67b922d37b4c5e51c058e7230ac2798a1b86c
Jigsy <34282672+Jigsy1@users.noreply.github.com>
2023-02-07 01:05:38 +0000

    Add files via upload
    One or two tweaks here and there.

--- py3/brute/brute_append_hash.py
+++ py3/brute/brute_append_hash.py
@@ -19,24 +19,30 @@ def rotate(index):
 
 # CHAR_MAP = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 # END_STRING = list(CHAR_MAP[-1] * len(CHAR_MAP))
-# >-> If you want the insanely long version, comment out CHAR_MAP and END_STRING below and uncomment the two above.
+# THIS_HASH = "c5f8c454ccd378437a4599aed72e1518"
+# >-> If you want the insanely long version, comment out CHAR_MAP, END_STRING and THIS_HASH below and uncomment the three above.
 CHAR_MAP = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-STRING = list(CHAR_MAP[0] * 1)
 END_STRING = list(CHAR_MAP[-1] * 3)
 
-THIS_HASH = "a61a40943e07257e084ea3f62dfdb1c8"
+THIS_HASH = "91164770fc34b877cc56ccaf61d6eee8"
+
+STRING = list(CHAR_MAP[0] * 1)
+# `-> Starting from the first character in the array: A
 
 def main():
+    print("Checking: {}".format(THIS_HASH))
+    print("{} character set ^ {} character(s) long = ~{} loop(s).\n".format(len(CHAR_MAP), len(END_STRING), pow(len(CHAR_MAP), len(END_STRING))))
     index = 0
     thisChar = 0
     while 1:
-        print("".join(STRING))
+        # print("".join(STRING))
+        # `-> You can speed up the entire program by commenting this line out.
         encodedString = "".join(STRING).encode("UTF-8")
         # `-> String needs to be encoded to use hashlib.
         hashOutput = hashlib.md5(encodedString).hexdigest()
         # `-> If you change the hash to sha1, sha256, etc, just remember to change this from md5.
         if hashOutput == THIS_HASH:
-            print("Match: {} -> {}".format("".join(STRING), hashOutput))
+            print("Match after {} loop(s): {} -> {}".format(thisChar, "".join(STRING), hashOutput))
             break
         if "".join(STRING) == "".join(END_STRING):
             break
@@ -48,6 +54,7 @@ def main():
         thisChar += 1
         STRING[index] = CHAR_MAP[thisChar % len(CHAR_MAP)]
         # time.sleep(1)
+        # `-> Uncomment this if you want a one second gap between checks.
     print("End")
 
 if __name__ == "__main__":
